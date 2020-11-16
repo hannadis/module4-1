378656fb64d35b5cc629f0afad8887af5add46e1
hannadis <hannadis90@gmail.com>
2020-11-16 23:01:53 +0000

    made all the changes

---
+++ index.js
@@ -0,0 +1,9 @@
+const logo = document.querySelectorAll("#logo path");
+
+
+
+for (let i = 0; i<logo.length; i++) {
+    console.log(`Letter ${i} is ${logo[i].getTotalLength()}`);
+}
+
+console.log(logo);
